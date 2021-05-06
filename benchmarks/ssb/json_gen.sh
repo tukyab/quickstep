e8b509fa4cda53c44f5221c651a3e7b3af4d1630
tsu <tsu@mit.edu>
2021-05-06 15:23:59 -0400

    reduce json gen time

--- benchmarks/ssb/json_gen.sh
+++ benchmarks/ssb/json_gen.sh
@@ -1,21 +1,35 @@
-for WORKERS in 2 5 10 30
+for DATA_SIZE in 2 5 10 20 50 80 100
 do
-	for DATA_SIZE in 2 5 10 20 50 80 100
+	WORKERS=2
+	echo "WORKERS=$WORKERS
+	DATA_SIZE=$DATA_SIZE
+	QS=/home/tenzin/quickstep/build/quickstep_cli_shell
+	QS_ARGS_NUMA_LOAD=''
+	QS_ARGS_NUMA_RUN=''
+	CREATE_SQL='create.sql'
+	LOAD_DATA=true
+	QUERIES='ALL'
+	QS_STORAGE=/ssd1/tenzin/ssb_qsstor/
+	QS_ARGS_BASE='-printing_enabled=false -num_workers=30'
+	QS_ARGS_BASE_RUN='-printing_enabled=false -tenzin_profiling=true -num_workers=$WORKERS'
+	DATA_PATH=/ssd1/tenzin/ssb_${DATA_SIZE}" > q.cfg
+
+	./run-benchmark.sh q.cfg
+	for WORKERS in 5 10 30
 	do
-	  echo "workers $WORKERS data $DATA_SIZE"
+
     echo "WORKERS=$WORKERS
     DATA_SIZE=$DATA_SIZE
     QS=/home/tenzin/quickstep/build/quickstep_cli_shell
     QS_ARGS_NUMA_LOAD=''
     QS_ARGS_NUMA_RUN=''
     CREATE_SQL='create.sql'
-    LOAD_DATA=true
+    LOAD_DATA=false
     QUERIES='ALL'
     QS_STORAGE=/ssd1/tenzin/ssb_qsstor/
     QS_ARGS_BASE='-printing_enabled=false -num_workers=30'
     QS_ARGS_BASE_RUN='-printing_enabled=false -tenzin_profiling=true -num_workers=$WORKERS'
     DATA_PATH=/ssd1/tenzin/ssb_${DATA_SIZE}" > q.cfg
-    cat q.cfg
 
 	  ./run-benchmark.sh q.cfg
 	done
